0b4b0279be1e948aa03ef6de3a403d8e7b5c70dc
AldhitL <AldhitL>
2017-10-24 17:22:25 +0700

    Serigala

---
+++ serigala.php
@@ -0,0 +1,15 @@
+<?php 
+class serigala {
+public $suara ;
+public $warna ;	
+public $kaki ;
+
+	public function __construct ($suara,$warna,$kaki)		{
+		
+		$this->suara = $suara ;
+		$this->warna= $warna ;
+		$this->kaki = $kaki ;
+		
+}
+}
+?>
---
+++ serigala1.php
@@ -0,0 +1,9 @@
+<?php
+require_once ('serigala.php');
+$serigala = new serigala ('AAAAuuuuuuuuu','Abu','4') ;
+echo "<center> =============================<br> <center> Serigala </center>============================= <br>" ;
+echo " Suara = ".$serigala->suara."<br>" ;
+echo " Warna = ".$serigala->warna."<br>" ;
+echo " Kaki = ".$serigala->kaki."<br>" ;
+
+?>
